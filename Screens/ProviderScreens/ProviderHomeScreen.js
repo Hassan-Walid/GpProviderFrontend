591b920434e1b2ba62e23a6e12bc8915c7aa450b
Peter Ashraf <peterashrafmail@gmail.com>
2024-06-24 03:26:00 +0300

    socket int

--- Screens/ProviderScreens/ProviderHomeScreen.js
+++ Screens/ProviderScreens/ProviderHomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import {
   Button,
   FlatList,
@@ -14,10 +14,10 @@ import SwitchStatus from "./../../components/ProviderComponents/SwitchStatus";
 import RequestScreen from "./RequestScreen";
 import { ProgressBar } from "react-native-paper";
 import io from "socket.io-client";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 
 const ProviderHomeScreen = ({ service, navigation /*route*/ }) => {
-  // const { socket } = route.params;
-  // console.log(socket);
 
   const [mapRegion, setMapRegion] = useState({
     latitude: 37.78825,
@@ -25,44 +25,108 @@ const ProviderHomeScreen = ({ service, navigation /*route*/ }) => {
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
-  const [mapRegion2, setMapRegion2] = useState({
-    latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  });
 
-  const [consumers, setConsumers] = useState([
-    { id: "1", name: "Provider 1", distance: "2 km", carType: "Sedan" },
-    { id: "2", name: "Provider 2", distance: "5 km", carType: "SUV" },
-    { id: "3", name: "Provider 3", distance: "3 km", carType: "Sedan" },
-    { id: "4", name: "Provider 4", distance: "7 km", carType: "SUV" },
-  ]);
+  // const [mapRegion2, setMapRegion2] = useState({
+  //   latitude: 37.78825,
+  //   longitude: -122.4324,
+  //   latitudeDelta: 0.0922,
+  //   longitudeDelta: 0.0421,
+  // });
+
+  // const [consumers, setConsumers] = useState([
+  //   { id: "1", name: "Provider 1", distance: "2 km", carType: "Sedan" },
+  //   { id: "2", name: "Provider 2", distance: "5 km", carType: "SUV" },
+  //   { id: "3", name: "Provider 3", distance: "3 km", carType: "Sedan" },
+  //   { id: "4", name: "Provider 4", distance: "7 km", carType: "SUV" },
+  // ]);
 
   const [isSwitchOn, setIsSwitchOn] = useState(false);
+  const [socket, setSocket] = useState(null);
+  const [id, setId] = useState(null);
+  const [type, setType] = useState(null);
+  const [pendingRequests, setPendingRequests] = useState([]);
+  const [isRequestAccepted, setIsRequestAccepted] = useState(false);
+  const [requestInfo, setRequestInfo] = useState({});
+
+  let intervalRef = useRef(null);
 
   useEffect(() => {
+    AsyncStorage.getItem("userId").then((data) => {
+      // console.log(data);
+      setId(data);
+
+    });
+
+    AsyncStorage.getItem("userRole").then((data) => {
+      // console.log(data);
+      setType(data);
+
+    });
     userLocation();
   }, []);
+
+  useEffect(() => {
+    if (isRequestAccepted) {
+      intervalRef.current = setInterval(() => {
+        console.log("tracked");
+        let { latitude, longitude } = mapRegion;
+        let { consumerId, consumerLocation } = requestInfo;
+        userLocation();
+        socket.emit("Tracked", { userId: id, targetId: consumerId, targetLocation: consumerLocation, location: { latitude, longitude } })
+      }, 3000)
+    }
+
+    return () => {
+      clearInterval(intervalRef.current);
+    }
+  }, [isRequestAccepted])
+
   useEffect(() => {
     if (isSwitchOn) {
-      const socket = io("http://192.168.1.5:8000");
-      // let x = socket.connect("http://192.168.1.5:8000");
-      socket.on("connect", () => {
+      let newsocket = io("http://192.168.1.10:8000");
+
+      newsocket.on("connect", () => {
         console.log("Connected to server");
-        socket.emit("connected", { id: "1", type: "provider" });
+        newsocket.emit("connected", { id, type });
       });
 
-      // socket.on('notification', (notificationData) => {
+      // newsocket.on('notification', (notificationData) => {
       //   console.log("Notification from server");
       //   console.log(notificationData);
       // })
 
-      // socket.on("GetLocation", ({ consumerId, providerId, availableProvidersLength }) => {
-      //   socket.emit("Location", { consumerId, providerId, availableProvidersLength, location })
-      // })
+      newsocket.on("GetLocation", ({ consumerId, providerId, availableProvidersLength }) => {
+        newsocket.emit("Location", { consumerId, providerId, availableProvidersLength, location: mapRegion })
+      })
+
+      newsocket.on('disconnect', () => {
+        console.log(`Disconnected from server`);
+      })
+
+      newsocket.on('IncomingRequest', async ({ consumerId, consumerLocation, distance }) => {
+
+        console.log("Incoming Request from " + consumerId + " at lat: " + consumerLocation + " distance: " + distance);
+
+        let incomingUser = await axios.get(`http://192.168.1.10:8000/api/user/${consumerId}`)
+          .then(res => {
+            return res.data.user;
+          })
+          .catch((e) => {
+            console.log(e);
+          })
+
+        setPendingRequests((allRequests) => [...allRequests, { consumerId, consumerLocation, distance, incomingUser }]);
+
+      })
+
+      setSocket(newsocket);
+
     } else {
-      // socket.disconnect();
+      if (socket) {
+        socket.emit('disconnected', { id, type })
+        socket.disconnect();
+        setSocket(null);
+      }
     }
   }, [isSwitchOn]);
 
@@ -84,8 +148,9 @@ const ProviderHomeScreen = ({ service, navigation /*route*/ }) => {
       });
     }
   };
+
+
   const [isOpened, setIsOpened] = useState(false);
-  const [isRequest, setIsRequest] = useState(true);
 
   const handleSwitchChange = (enabled) => {
     // console.log(enabled);
@@ -96,6 +161,8 @@ const ProviderHomeScreen = ({ service, navigation /*route*/ }) => {
       // socket.disconnect();
     }
   };
+
+
   const origin = { latitude: 37.78825, longitude: -122.4324 };
   const destination = { latitude: 37.79855, longitude: -122.4324 };
   const region = {
@@ -110,8 +177,8 @@ const ProviderHomeScreen = ({ service, navigation /*route*/ }) => {
       <View style={styles.mapContainer}>
         <MapView
           style={styles.map}
-          region={mapRegion}
-          // initialRegion={region}
+          region={origin}
+        // initialRegion={region}
         >
           <Marker coordinate={origin} title="Consumer place"></Marker>
           {/* <Marker coordinate={origin} title="Origin" />
@@ -133,14 +200,25 @@ const ProviderHomeScreen = ({ service, navigation /*route*/ }) => {
           <Text>Not Available For Request</Text>
         )}
 
-        {isRequest ? (
+        {pendingRequests.length > 0 ? (
           <View style={styles.consumersList}>
             {/* <ProgressBar progress={1}></ProgressBar> */}
-            <ConsumerCard
-              name="Consumer 1"
-              distance={"2 Km"}
-              carType={"Sedan"}
-            />
+            {pendingRequests.map((r, i) => {
+              return (
+                // <></>
+                <ConsumerCard
+                  key={i}
+                  consumerId={r["consumerId"]}
+                  consumerLocation={r["consumerLocation"]}
+                  name={r["incomingUser"]["name"].toUpperCase()}
+                  carType={r["incomingUser"]["owned_cars"][0]["make"].toUpperCase() + " " + r["incomingUser"]["owned_cars"][0]["model"].toUpperCase()}
+                  distance={+r.distance.toFixed(2) + "KM"}
+                  setIsRequestAccepted={setIsRequestAccepted}
+                  setRequestInfo={setRequestInfo}
+                  setPendingRequests = {setPendingRequests}
+                />
+              )
+            })}
           </View>
         ) : (
           <Text></Text>
--- components/ProviderComponents/ConsumerCard.js
+++ components/ProviderComponents/ConsumerCard.js
@@ -5,44 +5,53 @@ import CustomButton from "../CustomButton";
 
 const LeftContent = (props) => <Avatar.Icon {...props} icon="account" />;
 
-const ConsumerCard = ({ name, distance, carType, navigation }) => (
-  <View style={styles.cardContainer}>
-    <Card style={styles.card}>
-      <Card.Content>
-        <View style={styles.row}>
-          <Avatar.Icon icon="account" size={48} style={styles.avatar} />
-          <View style={styles.textContainer}>
-            <Text variant="titleLarge" style={styles.title}>
-              {name}
-            </Text>
-            <Text variant="bodyMedium" style={styles.subtitle}>
-              {distance}
-            </Text>
-            <Text variant="bodyMedium" style={styles.carType}>
-              {carType}
-            </Text>
+const ConsumerCard = ({ name, distance, carType, consumerId, consumerLocation,  setIsRequestAccepted, setRequestInfo, setPendingRequests }) => {
+  
+  let handleAccept = () => {
+    setIsRequestAccepted(true);
+    setPendingRequests([]);
+    setRequestInfo({consumerId, consumerLocation})
+  }
+
+  return (
+    <View style={styles.cardContainer}>
+      <Card style={styles.card}>
+        <Card.Content>
+          <View style={styles.row}>
+            <Avatar.Icon icon="account" size={48} style={styles.avatar} />
+            <View style={styles.textContainer}>
+              <Text variant="titleLarge" style={styles.title}>
+                {name}
+              </Text>
+              <Text variant="bodyMedium" style={styles.subtitle}>
+                {distance}
+              </Text>
+              <Text variant="bodyMedium" style={styles.carType}>
+                {carType}
+              </Text>
+            </View>
           </View>
-        </View>
-      </Card.Content>
-      <Card.Actions style={styles.actions}>
-        <View style={styles.button}>
-          <View style={{ width: "40%" }}>
-            <CustomButton
-              title={"Accept"}
-              onPressHandler={() => {}}
-            ></CustomButton>
+        </Card.Content>
+        <Card.Actions style={styles.actions}>
+          <View style={styles.button}>
+            <View style={{ width: "40%" }}>
+              <CustomButton
+                title={"Accept"}
+                onPressHandler={handleAccept}
+              ></CustomButton>
+            </View>
+            <View style={{ width: "40%" }}>
+              <CustomButton
+                title={"Skip"}
+                onPressHandler={() => { }}
+              ></CustomButton>
+            </View>
           </View>
-          <View style={{ width: "40%" }}>
-            <CustomButton
-              title={"Skip"}
-              onPressHandler={() => {}}
-            ></CustomButton>
-          </View>
-        </View>
-      </Card.Actions>
-    </Card>
-  </View>
-);
+        </Card.Actions>
+      </Card>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   cardContainer: {
